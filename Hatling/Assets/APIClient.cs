0fbefc7e43b255e0095b9882ab28c50f414ca4be
Youil Samara <youilsamara@gmail.com>
2017-11-06 14:46:58 -0200

    Adicionando unity

---
+++ Hatling/Assets/APIClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.Networking;
+ 
+ public class APIClient : MonoBehaviour
+ {
+     const string url = "http://localhost:50003/API/Personagems/";
+ 
+     // Use this for initialization
+     void Start()
+     {
+         StartCoroutine(GetPersonagemsAPI());
+     }
+ 
+     IEnumerator GetPersonagemsAPI()
+     {
+         //UnityWebRequest request = UnityWebRequest.Get(url + "/1");
+         UnityWebRequest request = UnityWebRequest.Get(url);
+ 
+         yield return request.Send();
+ 
+         if (request.isNetworkError || request.isHttpError)
+         {
+             Debug.Log(request.error);
+         }
+         else
+         {
+             string strRespostaServidor = request.downloadHandler.text;
+             Debug.Log(strRespostaServidor);
+ 
+             byte[] result = request.downloadHandler.data;
+
+            //Item meuItem = JsonUtility.FromJson<Item>(strRespostaServidor);
+            //ImprimirItem(meuItem);
+
+
+            Personagem[] teste = JsonHelper.getJsonArray<Personagem>(strRespostaServidor);
+
+            foreach (Personagem i in teste)
+            {
+                ImprimirPersonagem(i);
+            }
+        }
+     }
+ 
+     void ImprimirPersonagem(Personagem i)
+     {
+         Debug.Log("====== Dados objeto ======= ");
+         Debug.Log("ID: " + i.PersonagemID);
+         Debug.Log("Nome: " + i.Nome);
+         Debug.Log("Descrição: " + i.Descricao);
+ 
+     }
+ }
---
+++ Hatling/Assets/Personagem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+[System.Serializable]
+public class Personagem
+{
+
+    public int PersonagemID;
+
+    public string Nome;
+
+    public string Descricao;
+
+    public string ComidaFavorita;
+
+    public int Altura;
+
+}
